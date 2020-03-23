5382c22a50a9b0a80f2c8350118db57430063271
raluque <ralubalas@yahoo.com>
2020-03-23 12:09:32 +0200

    3rd commit

--- jdk-write-a-simple-java-program-raluque/src/main/java/RaluTest.java
+++ jdk-write-a-simple-java-program-raluque/src/main/java/RaluTest.java
@@ -2,5 +2,6 @@ public class RaluTest {
     public static void main (String[] args){
         System.out.println("Hello first");
         System.out.println("How you doin");
+        System.out.println("Goodbye");
     }
 }
